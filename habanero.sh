c2636262c5613d8f6f070c7751fac8b10f56e046
root <edward>
2014-09-14 21:42:02 -0700

    script will still run even userforgets to create log folder

--- habanero.sh
+++ habanero.sh
@@ -6,5 +6,6 @@
 # Or to run headless...
 #       ./habanero.sh -config CONFIGNAME -detached
 #
+mkdir -p log 
 : ${config:=habanero}
 erl -pa ebin deps/*/ebin -args_file vm.args -config ${config} $@
